2a010f33797609416e5cc1c768e289ee473df804
Dan Bock <danbock@gmail.com>
2014-12-10 16:41:31 -0500

    Multiple changes

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+CRYPT_BLOWFISH or die ('No Blowfish found.');
+define("BLOWFISH_PRE", "$2y$05$");
+define("BLOWFISH_SUF", "$");
+
+if(!isset($_SERVER['SERVER_NAME'])) {
+  exit("No server name found");
+}
+
+if($_SERVER['SERVER_NAME'] == "danbock.net") {
+  define("ENVIRONMENT", "dev");
+  define("DATABASE_NAME", "topbidfa_orange");
+}
+else {
+  exit("No environment.");
+}
+
