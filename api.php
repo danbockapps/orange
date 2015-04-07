03eb0e10cc53b84716f3427b257f539bc73f2643
Dan Bock <danbock@gmail.com>
2015-04-07 14:05:20 -0400

    Extend sessions to 30 days

--- api.php
+++ api.php
@@ -1,4 +1,6 @@
 <?php
+ini_set("session.cookie_lifetime", 60*60*24*30);
+ini_set("session.gc_maxlifetime", 60*60*24*30);
 session_start();
 require_once("php/config.php");
 $contents = file_get_contents("php://input");
