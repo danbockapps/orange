df567642ad95ac6b71bb986c7546abdf8d3f3d86
Dan Bock <danbock@gmail.com>
2015-04-13 13:51:13 -0400

    Remove test users from report

--- php/report_currentusers.php
+++ php/report_currentusers.php
@@ -8,10 +8,12 @@ $ok_array['users'] = pdo_select("
     u.lname,
     u.email
   from users u
-  where u.userid in (
-    select userid
-    from current_team_members
-  )
+  where
+    !u.testuser
+    and u.userid in (
+      select userid
+      from current_team_members
+    )
   order by
     u.lname,
     u.fname
